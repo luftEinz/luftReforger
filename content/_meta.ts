e32080c5a0ec73927658d1e5ecbf7b23a6fd246d
luftEinz <luft0938@gmail.com>
2025-05-14 11:18:56 -0300

    Initial commit

---
+++ content/_meta.ts
@@ -0,0 +1,30 @@
+export default {
+  index: {
+    title: 'Home',
+  },
+  '##': {
+    type: 'separator',
+    title: 'Separator', // Title is optional
+  },
+  mantine: {
+    title: '🌳 Mantine',
+  },
+  '###': {
+    type: 'separator',
+    // title: 'My Items', // Title is optional
+  },
+  versioning: {
+    title: 'Versioning',
+    theme: {
+      layout: 'full',
+    },
+  },
+  'release-notes': {
+    title: 'Release Notes',
+    theme: {
+      toc: false,
+      // sidebar: false,
+      layout: 'full',
+    },
+  },
+};
